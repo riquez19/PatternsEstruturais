8a3544e60c3dadd96e3bd8c69779029e78496037
riquez19 <henrique_affonso@hotmail.com>
2018-03-04 18:15:22 -0300

    Base das classes

---
+++ projeto_qualidade/src/projeto_qualidade/chamado.java
@@ -0,0 +1,10 @@
+package projeto_qualidade;
+
+public class chamado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ projeto_qualidade/src/projeto_qualidade/fila.java
@@ -0,0 +1,5 @@
+package projeto_qualidade;
+
+public class fila {
+
+}
